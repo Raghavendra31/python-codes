96a1db7fb454dc2f7a4a8e5f1785006f24614f52
Raghavendra31 <r10756521@gmail.com>
2025-08-12 23:04:40 +0530

    https://github.com/Raghavendra31/python-codes.git

---
+++ list_in_py.py
@@ -0,0 +1,28 @@
+items=["milk", "suger", "bru"]
+
+print ( items )
+print (items[1])
+
+items.pop(1)
+
+print(items)
+
+items.append("kushka")
+print (items)
+
+# remove 
+#insert
+
+items.remove("kushka")
+items.insert(2,"motte")
+
+print (items)
+
+items.clear()
+
+print (items)
+    
+    #error 
+lists = [1, "ragu" ,3]
+
+print (sum(lists))
