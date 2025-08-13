0e0ab91c0025ab1057dfd6d4886af1b0400786a7
Raghavendra31 <r10756521@gmail.com>
2025-08-14 01:28:24 +0530

    learnt dictionary and for loop

--- dictionary.py
+++ dictionary.py
@@ -4,28 +4,67 @@
 
 }'''
 
-date = {
-    "ragu":"15/7/2005",
-    "mallika":"20/1/1984"
-}
+# date = {
+#     "ragu":"15/7/2005",
+#     "mallika":"20/1/1984"
+# }
 
-print (date["ragu"])
-print (date ["mallika"])
+# print (date["ragu"])
+# print (date ["mallika"])
 
-# know about get keyword
+# # know about get keyword
 
-print(date.get("ronaldo", "couldn't find"))
+# print(date.get("ronaldo", "couldn't find"))
 
-item1={
-    "name":"milk",
-    "weight":1
-}
+# item1={
+#     "name":"milk",
+#     "weight":1
+# }
 
-item2 = {
-    "name":"sugar",
-    "weight":2
-}
+# item2 = {
+#     "name":"sugar",
+#     "weight":2
+# }
 
-items=[ item1,item2,]
-print (items)
-print (f'total weigth = {item1["weight"] + item2 ["weight"] }kg')
+# items=[ item1,item2,]
+# print (items)
+# print (f'total weigth = {item1["weight"] + item2 ["weight"] }kg')
+
+
+# d = {"bengaluru": "vade",
+#      "mumbai": "vada pav",
+#      "chennai": "idli",
+#      "delhi": "chole bhature"}
+
+# d.update({"hyderabad": "biryani"})  # Adding a new key-value pair
+
+# d.pop("mumbai")
+# # i want to change the dish of bengaluru to dosa
+# d["bengaluru"] = "dosa"
+# # using the key() method to get all keys
+# key = d.values()
+# print(key)
+
+
+# nested dictionary example
+
+# n = {"friend 1" : {"name": "Raghavendra", "age": 30, "city": "Bangalore"},
+#         "friend 2": {"name": "Mallika", "age": 29, "city": "Chennai"},
+#         "friend 3": {"name": "Kumar", "age": 32, "city": "Hyderabad"}}
+
+# for friend, details in n.items():
+#      print(f"{friend}:")
+#      for key, value in details.items():
+#          print(f"  {key}: {value}")
+#      print()  # Print a newline for better readability
+# print(f"city of friend 1 is {n['friend 2']['city']}")
+
+d = {"bru": 1,
+     "nescafe": 2,
+     "tata": 3,
+     "coffee day": 4}
+
+for key, value in d.items():
+    #sum of all values
+    total = sum(d.values())
+print (total)
--- for_loop.py
+++ for_loop.py
@@ -11,6 +11,67 @@
 #     print (f"index: {index}, value: {value}")
 
 
-for i in range(1, 11):
-    for j in range(1, 11):
-        print(f"{i} X {j} = {i * j}")
+# for i in range(1, 11):
+#     for j in range(1, 11):
+#         print(f"{i} X {j} = {i * j}")
+
+
+# a= [1 , 23,54 ,55 , 67, 78, 89, 90, 100]
+# total = 0
+# for i in a:
+#     total += i
+# print(f"Total sum of the list is: {total}")
+
+
+# a = {"Name": "Raghavendra",
+#      "Age": 30,
+#      "City": "Bangalore",
+#      "Occupation": "Software Engineer"}
+
+# for key, value in a.items():
+#     print(f"{key}: {value}")
+
+# a = ["raghavendra", "kumar", "reddy"]
+# marks = [90, 80, 70]
+# total = {}
+# for index, name in enumerate(a):
+#     total[name]= marks [index]
+# print(total)
+
+
+# comphrehension examples
+
+
+# l = [1, 2, 3, 4, 5]
+# dl = [item**2 for item in l]  # dl = { equation for item in collection }
+# print(dl)
+
+# for i in range(1,11):
+#     dl = i**2 
+#     print(dl,end=" ")
+
+
+# new comprehension example
+
+# l = [1, 2, 3, 4, 5]
+# dl = [item**2 for item in l if item % 2 != 0]  # Only squares of even numbers
+# print(dl)
+
+# a = ["raghavendra", "kumar", "reddy"]
+
+# cl =[item[1] for item in a ]
+# print (cl)
+
+
+# a = ["raghavendra", "kumar", "reddy"]
+
+# da = {item : len(item) for item in a}
+# print(da)
+# dda = {}
+# for index, item in enumerate(a):
+#     dda[item] = len(item)
+# print(dda)
+
+a= input("Enter a list of numbers separated by spaces: ").split()
+
+print (a)
