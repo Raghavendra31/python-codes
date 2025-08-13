c31c45817802955cba3457e67274f4f4942f85d9
Raghavendra31 <r10756521@gmail.com>
2025-08-13 14:02:14 +0530

    #if, elif and else statement

---
+++ loops.py
@@ -0,0 +1,18 @@
+#if,elif and else statements 
+
+x = 11
+
+if x==10 :
+    print ("x is 10")
+else:
+    print ("x is " + str(x))
+
+
+signal = input("enter the signal : ")
+
+if signal =="red":
+    print ("stop")
+elif signal == "yellow":
+    print ("ready to go")
+else:
+    print ("go")
