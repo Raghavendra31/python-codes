027f634e0e9529cc131a7c236752eb595d494901
Raghavendra31 <r10756521@gmail.com>
2025-08-17 01:45:06 +0530

    modified class and encapsulation

--- encapsulation.py
+++ encapsulation.py
@@ -4,20 +4,28 @@
 # encaplsualtion is the proess of wrapping the attributes and methods of an object
 
 
-class db:
-    def __init__(self):
-        self.__storage = {}
+# class db:
+#     def __init__(self):
+#         self.__storage = {}
 
-    def write(self,key,value):
-        self.__storage[key] =value
+#     def write(self,key,value):
+#         self.__storage[key] =value
     
-    def read(self,key):
-        if key in self.__storage:
-            return self.__storage[key]
-        else:
-            return "Key not found"
+#     def read(self,key):
+#         if key in self.__storage:
+#             return self.__storage[key]
+#         else:
+#             return "Key not found"
 
 
-r = db()
-r.write("name", "John Doe")
-print(r.read("ragu"))
+# r = db()
+# r.write("name", "John Doe")
+
+
+def swap_case(s):
+    return s.swapcase()
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
