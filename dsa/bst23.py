f9d710eeac186cf470a66100c7cc8b79ec6d3d2e
Raghavendra31 <r10756521@gmail.com>
2026-03-06 00:18:48 +0530

    bst rev

--- dsa/bst23.py
+++ dsa/bst23.py
@@ -19,14 +19,57 @@ class bst:
         while True:
             if new_node.data == temp.data:
                 print("node already exists")
-                return
+                return False
+            
             elif new_node.data < temp.data :
-                temp.left = new_node
+                if temp.left is None:
+
+                    temp.left = new_node
+                    return True 
+                temp = temp.left
+            
+            else: 
+                if temp.right is None:
+                    temp.right = new_node
+                    return True
+                temp = temp.right
+    
+
+    def checkNode(self,value):
+
+        if self.root is None:
+            return False
+        
+        temp = self.root
+
+        while temp is not None :
+            if value <temp.data:
+                temp = temp.left
                 
-                return
             
-            elif new_node.data > temp.data:
-                temp.right = new_node
-                return
+            elif value > temp.data:
+                temp = temp.right
+                
             
-            
+            else:
+                return True
+
+        return False
+
+
+
+
+r = bst()
+
+r.insert(26)
+r.insert(65)
+r.insert(15)
+r.insert(75)
+r.insert(46)
+
+
+print(r.checkNode(2))
+print(r.checkNode(43))
+print(r.checkNode(23))
+print(r.checkNode(26))
+print(r.checkNode(15))
