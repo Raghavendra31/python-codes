af20bfb144a1a336a6cd2d432e1bb8bab6d6ad5b
Raghavendra31 <r10756521@gmail.com>
2026-03-06 01:41:30 +0530

    leetcode

--- leetcodes/two_sum.py
+++ leetcodes/two_sum.py
@@ -1,11 +1,13 @@
-ragu = input()
-nums = list(map(int, ragu.strip("[]").split(",")))
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        for i in range(0,len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
 
+r = Solution()
 
+nums = eval(input())   # converts "[2,7,11,15]" → [2,7,11,15]
 target = int(input())
 
-for i in range( 0,len(nums)):
-    for j in range( 0,len(nums) ):
-        if (nums[i] + nums[j] == target):
-            print (f"[{i},{j}]")
-
+print(r.twoSum(nums, target))
