f101a2cf1055a43f281ce6ab0c8e3bbc2106e952
Raghavendra31 <r10756521@gmail.com>
2026-03-10 19:40:57 +0530

    sum of two num

--- leetcodes/sum_two_num.py
+++ leetcodes/sum_two_num.py
@@ -1,102 +1,27 @@
-# Definition for singly-linked list.
 class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+    def __init__(self,val):
+        self.val = val 
+        self.next = None
 
 
-class Solution:
+class Solution(object): # Added (object) for Python 2 compatibility
     def addTwoNumbers(self, l1, l2):
-        dummy = ListNode()      
+        dummy = ListNode(0)
         current = dummy
         carry = 0
-
+        
         while l1 or l2 or carry:
-
-            val1 = l1.val if l1 else 0
-            val2 = l2.val if l2 else 0
-
-            total = val1 + val2 + carry
+            # Use getattr or a simple if/else for safety
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+            
+            total = v1 + v2 + carry
             carry = total // 10
-            digit = total % 10
-
-            current.next = ListNode(digit)
+            
+            current.next = ListNode(total % 10)
+            
             current = current.next
-
-            if l1:
-                l1 = l1.next
-            if l2:
-                l2 = l2.next
-
+            if l1: l1 = l1.next
+            if l2: l2 = l2.next
+            
         return dummy.next
-    
-class Node:
-    def __init__(self, val):
-        self.val = val   # store data
-        self.next = None   # initially no next node
-class LinkedList:
-    def __init__(self):
-        self.head = None   # start with an empty list
-
-    # Insert at the end
-    def append(self, data):
-        new_node = Node(data)
-        if self.head is None:
-            self.head = new_node
-            return
-        temp = self.head
-        while temp.next:
-            temp = temp.next
-        temp.next = new_node
-
-    # Print the list
-    def display(self):
-        temp = self.head
-        while temp:
-            print(temp.data, end=" -> ")
-            temp = temp.next
-        print("None")
-# Create linked list
-ll = LinkedList()
-
-# Add elements
-ll.append(1)
-ll.append(2)
-ll.append(3)
-
-
-class LinkedList1:
-    def __init__(self):
-        self.head = None   # start with an empty list
-
-    # Insert at the end
-    def append(self, data):
-        new_node = Node(data)
-        if self.head is None:
-            self.head = new_node
-            return
-        temp = self.head
-        while temp.next:
-            temp = temp.next
-        temp.next = new_node
-
-    # Print the list
-    def display(self):
-        temp = self.head
-        while temp:
-            print(temp.data, end=" -> ")
-            temp = temp.next
-        print("None")
-# Create linked list
-
-l2 = LinkedList1()
-
-l2.append(4)
-l2.append(5)
-l2.append(6)
-
-r = Solution()
-
-
-
-r.addTwoNumbers(ll,l2)
