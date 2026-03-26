8c1229b38a460704da0c3e77e21dd9bea2e1cc77
Raghavendra31 <r10756521@gmail.com>
2026-03-26 16:47:26 +0530

    prac

--- hacker_rank/capital.py
+++ hacker_rank/capital.py
@@ -1,14 +1,30 @@
-import os
-def solve(s):
-    r, t = s.split(" ")
-    r = r[0].upper() + r[1:]
-    t = t[0].upper() + t[1:]
-    print(r, t)
+# import os
+# def solve(s):
+#     r, t = s.split(" ")
+#     r = r[0].upper() + r[1:]
+#     t = t[0].upper() + t[1:]
+#     print(r, t)
 
 
 
-s = input()
+# s = input()
 
-solve(s)
+# solve(s)
+
+def merge_the_tools(string, k):
+    n = len(string)
+    
+    for i in range(0,n,k):
+        substring = string[i:i+k]
+        s = set() 
+        result = ""
+        
+        for char in substring:
+            if char not in set:
+                set.add(char) 
+                result += char  
+        print(result)    
 
 
+merge_the_tools("raguarhuq",3)
+    
---
+++ hacker_rank/minionGame.py
@@ -0,0 +1,5 @@
+s = "ragu"
+
+print( s[1] in ("a","s","d","f","g","h"))
+
+print("a" == "a")
